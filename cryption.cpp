66c6cc16bf7fff9b8761fdcd3d46c6f068108431
Heather Morgan <hmorgan@nmsu.edu>
2018-03-19 09:30:52 -0600

    cryption.cpp
    created some base outlines of functions.

---
+++ cryption.cpp
@@ -0,0 +1,27 @@
+#include "cryption.h"
+
+//constructors
+cryption(){
+
+
+}
+
+// get function
+String c(int n, string filename){
+switch(n) {
+    case 0:
+    break;
+    case 1 :
+    break;
+    default:
+    std::cout << "invalid entry"; 
+    break;}
+
+}
+// set functions
+void modifyIV(String cIV){
+
+}
+void modifyKey(String cKey){
+
+}
