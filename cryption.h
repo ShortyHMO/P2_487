3074e264fc1f4566d696658c555ee8d30ae114a2
Heather Morgan <hmorgan@nmsu.edu>
2018-03-19 09:28:06 -0600

    Created cryption.h
    basic outline in comments.

---
+++ cryption.h
@@ -0,0 +1,25 @@
+//Cryption
+/*
+Global Values:
+key 
+filename
+sbox
+IV
+Pbox
+
+
+//constrctor:
+cryption()
+
+// get
+String c(int n, string filename)
+// set
+void modifyIV(string cIV)
+void modifyKey(string cKey)
+//modify
+round function()
+keySchedule()
+cbc()// cipeher -block chaining
+permutation();
+*/
+
