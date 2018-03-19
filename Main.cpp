deeec7b503ba80edeadf7dbba6e6324a140b4845
Heather Morgan <hmorgan@nmsu.edu>
2018-03-19 09:18:24 -0600

    Basic outline of main
    has a base outline

---
+++ Main.cpp
@@ -0,0 +1,14 @@
+// main
+/*
+break down so far
+operation
+key
+input file
+auto generate key,IV,if not fiven 
+try catch errors
+
+Create Class object
+class object functions public:
+modifyIV(string), modifyKey(string), c(int,string);
+
+*/
