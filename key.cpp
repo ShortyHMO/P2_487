6c1d38103065d48e89bc6068fc2d93195e466515
ShortyHMO <hmorgan@nmsu.edu>
2018-04-08 23:57:28 -0600

    adjustment to Key

--- key.cpp
+++ key.cpp
@@ -1,1 +1,70 @@
 //key.cpp
+
+
+void key(string k){
+  int a;
+  char b;
+  char c;
+  stringstream hex;
+  string h;
+while(a <112){
+  b = x.charAt(a);
+  c = x.charAt(a+1);
+  hex <<b << c;
+  h = hex.str();
+  mainKey.at(a) = stoi(h,nullptr,16);
+  a = a +2;
+}
+
+}
+vector <unsigned int> kofi{
+r++;
+round();
+	return subkey;
+}
+
+void shift(){
+
+switch(r){
+	case 1: case 2: case 9: case 16:
+	lKStart++;
+	break;
+	default:
+	index+2;
+	break;
+}
+	lKStart = lKStart%28; 
+	rKStart = (lKStart+28)%28;
+}
+void round(){
+	shift();
+	compress();
+
+}
+void compress(){
+	
+	int lk =lKStart; 
+	int rk = rKStart;
+	int check = 0;
+	for(int i = 0; i<24; i++){
+	  r = i+24;
+	  subkey.at(i) = mainKey.at(i+lk);
+	  subkey.at(i) = mainKey.at(i+rk);
+	}
+	check = subkey.size();
+	if(check != 48){
+	    cerr << "There is an error in obtaining a subkey."<<"\n";
+	    exit(1);	
+	}
+
+}
+vector<unsigned int> fKey(){
+	
+	for(int k =0; k<28; k++){
+	 finalKey.at(k) = mainKey.at(K+lKStart);
+	 finalKey.at(k) = mainKey.at(k+rKStart);
+	}
+	return finalKey;
+
+}
+
--- key.h
+++ key.h
@@ -1,6 +1,20 @@
 //key.h
 
+class key{
 
-
+vector<unsigned int> mainKey (56);
+vector<unsigned int> finalKey(56);
+vector <unsigned int> subkey (48);
+int r = 0;
+int lKStart;// this is where the vector will start; 
+int rKStart;
+public:
+void key(string k);
+vector <unsigned int>kofi();
+private:
+void shift();
+void round();
+void compress();
+}
 
 
